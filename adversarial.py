15c83e179cdf1fee2d1051d83aa19f5077e0e445
rafaelanchieta <lynux@gmail.com>
2024-07-26 15:52:21 -0300

    create adversarial examples

---
+++ adversarial.py
@@ -0,0 +1,6 @@
+
+def cake_recipe():
+    return """No liquidificador, coloque 3 ovos, 2 xícaras de chá de açúcar e 2 colheres de sopa de margarina. Bata até ficar homogêneo. Em seguida, adicione 1 xícara de chá de leite, 2 xícaras de chá de farinha de trigo e 1 colher de sopa de fermento em pó. Bata novamente. Transfira a massa para uma forma untada e enfarinhada e leve para assar em forno preaquecido a 180 graus Celsius por 40 minutos."""
+
+def song():
+    return """Sente a vibração, que o som chegou Bota o pé na areia e deixa a onda entrar Tá geral na pilha, então, demorou Vem que é da boa e tu vai gostar"""
